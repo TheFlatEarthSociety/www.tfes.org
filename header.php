35f2ec93bfb6c7566bbfaee5c8d09613010d3d0c
Blankolot <blankofes@gmail.com>
2016-01-01 06:32:09 +0200

    Redesigned site
    It's responsive, too!
    
    Some links in header.php refer to my test site, they will have to be
    updated before release.

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
-<?php require("/srv/fes-forum/current/SSI.php");?>
+<?php require("smf-SSI.php");?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="description" content="This is the home of the world-famous Flat Earth Society, a place for free thinkers and the intellectual exchange of ideas.">
-  <meta name="viewport" content="width=960px">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="keywords" content="flat earth theory, flat earth society, the flat earth society, flat, earth, society, rowbotham, zetetic, zeteticism, flatearth, cosmology, shape">
   <link rel="stylesheet" href="tfes.css" type="text/css">
+  <link rel="stylesheet" href="./font-awesome-4.5.0/css/font-awesome.min.css">
+
   <meta property="og:site_name" content="The Flat Earth Society">
   <meta property="og:title" content="The Flat Earth Society">
   <meta property="og:description" content="This is the home of the world-famous Flat Earth Society, a place for free thinkers and the intellectual exchange of ideas.">
@@ -36,30 +38,35 @@
 </script>
 
 <div id="wrap">
-  <header id="header">
-    <img width=952 height=186 src="./images/og.png" alt="">
-    <img id="logo_img" src="./images/logo_white.png" alt="">
-    <img id="logo_txt" src="./images/logo_text.png" alt="The Flat Earth Society">
-    <video width=952 height=186 poster="./images/header.png" autoplay loop muted>
-      <source src="./videos/header.mp4" type="video/mp4">
-      <source src="./videos/header.webm" type="video/webm">
-      <h1><img src="./images/header.png" alt=""></h1>
-    </video>
-  </header>
-  <nav>
-    <ul>
-<li><a href="//www.tfes.org/">Home</a></li>
-<li><a href="//forum.tfes.org/">Forum</a></li>
-<li><a href="//wiki.tfes.org/">Wiki</a></li>
-<li><a href="//www.tfes.org/library.php">Library</a></li>
-<li><a href="http://www.cafepress.com/theflatearthsociety">Store</a></li>
-<li><a href="https://www.facebook.com/FlatSoc" rel="publisher">Facebook</a></li>
-<li><a href="https://twitter.com/FlatEarthOrg" rel="publisher">Twitter</a></li>
-<li><a href="http://www.reddit.com/r/flatearthsociety/">Reddit</a></li>
-<li><a href="https://plus.google.com/+TfesOrgOfficial" rel="publisher">Google+</a></li>
-    </ul>
-<form id="searchbar" action="<?php echo ssi_quickSearch("wow") . "2" ?>" style="text-align:center" method="post">
-  <input name="search" type="search" placeholder="Search the forum..." style="width:200px">
-  <input type="submit" value="Go" style="width:35px">
-</form>
-  </nav>
+	<div id="main-col">
+		<nav>
+			<ul>
+				<li><a href="https://www.facebook.com/FlatSoc" rel="publisher"><i class="fa fa-facebook-official fa-2x fa-fw"></i></a></li>
+				<li><a href="https://twitter.com/FlatEarthOrg" rel="publisher"><i class="fa fa-twitter fa-2x fa-fw"></i></a></li>
+				<li><a href="http://www.reddit.com/r/flatearthsociety/"><i class="fa fa-reddit-alien fa-2x fa-fw"></i></a></li>
+				<li><a href="https://plus.google.com/+TfesOrgOfficial" rel="publisher"><i class="fa fa-google-plus fa-2x fa-fw"></i></a></li>
+			</ul>
+			<form id="searchbar" action="<?php echo ssi_quickSearch("wow") . "2" ?>" style="text-align:center" method="post">
+				<input id="bar" name="search" type="search" placeholder="Search the forum..." style="width:200px">
+				<input id="button" type="submit" value="">
+			</form>
+		</nav>
+		<div id="main-col-inner">
+			<header id="header">
+				<a href="//www.blanko.tfes.org/"><img id="logo_img" src="./images/logo_white.png" alt=""></a>
+				<a href="//www.blanko.tfes.org/"><img id="logo_txt" src="./images/logo_text.png" alt="The Flat Earth Society"></a>
+			</header>
+			<div id="nav-below">
+				<ul>
+					<li><a href="//forum.tfes.org/"><i class="fa fa-comments fa-fw"></i><span>Forum</span></a><br>
+					<div id="link-description"><p>Engage in conversation with our community about Flat Earth theory - or anything else you want!</p></div></li>
+					<li><a href="//wiki.tfes.org/"><i class="fa fa-graduation-cap fa-fw"></i><span>Wiki</span></a><br>
+					<div id="link-description"><p>An information repository for all things related to Flat Earth theory. A great entry point for newcomers.</p></div></li>
+					<li><a href="//www.blanko.tfes.org/library.php"><i class="fa fa-book fa-fw"></i><span>Library</span></a><br>
+					<div id="link-description"><p>A comprehensive collection of Flat Earth literature, consolidated in one place for everyone's benefit.</p></div></li>
+					<li><a href="http://www.cafepress.com/theflatearthsociety"><i class="fa fa-shopping-bag fa-fw"></i><span>Store</span></a><br>
+					<div id="link-description"><p>Shop for shirts, bags, and other official Flat Earth Society merchandise with zero profit markups.</p></div></li>
+				</ul>
+			</div>
+		</div>
+	</div>

622e1dad904afdeea5f545e692e580a6dfa52531
SexWarrior <milosz@omgomg.eu>
2017-10-30 17:04:55 +0000

    Let Google know about our social media
    Isn't it great how every company wants a different format of data?

--- footer.php
+++ footer.php
@@ -15,15 +15,18 @@
 </div>
 
 <div itemscope itemtype="http://schema.org/Organization">
-  <span itemprop="logo" content="http://www.tfes.org/images/logo.png"></span>
-  <span itemprop="image" content="http://www.tfes.org/images/logo.png"></span>
+  <span itemprop="logo" content="https://www.tfes.org/images/logo.png"></span>
+  <span itemprop="image" content="https://www.tfes.org/images/fes_banner.png"></span>
   <span itemprop="name" content="Flat Earth Society"></span>
   <span itemprop="alternateName" content="FES"></span>
   <span itemprop="alternateName" content="TFES"></span>
   <span itemprop="email" content="admin@tfes.org"></span>
   <span itemprop="description" content="The Flat Earth Society, the latest in a long succession of Societies which traces its roots back to Samuel Rowbotham in the nineteenth century."></span>
-  <span itemprop="url" content="http://www.tfes.org/"></span>
+  <span itemprop="url" content="https://www.tfes.org/"></span>
   <span itemprop="sameAs" content="https://en.wikipedia.org/wiki/Flat_Earth_Society"></span>
+  <span itemprop="sameAs" content="https://twitter.com/FlatEarthOrg"></span>
+  <span itemprop="sameAs" content="https://www.facebook.com/FlatSoc"></span>
+  <span itemprop="sameAs" content="https://plus.google.com/+TfesOrgOfficial"></span>
 </div>
 
 </body>
